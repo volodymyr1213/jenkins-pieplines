68ec8c36699bf8a4bafcb238725aa5542a69b874
volodymyr1213 <vova.khomenko.1990@gmail.com>
2019-03-16 11:45:21 -0500

    added scm

--- jenkins-file.groovy
+++ jenkins-file.groovy
@@ -1,7 +1,7 @@
 
 node {
 properties([parameters([string(defaultValue: '127.0.0.1', description: 'Please give IP to host a website ', name: 'DEVIP', trim: true)])])
-
+properties([pipelineTriggers([pollSCM('* * * * *')])])
     stage("Pull git"){
         git "git@github.com:volodymyr1213/website.git"
 }
